5d55cb1af4fb6b9b288fd72837701c0e5b85ec83
Emily Yu <bluerainmango@gmail.com>
2019-11-03 12:24:04 -0800

    pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+// at least one type should be selected. prompt
+    // length ( 8 - 128 )
+    // special cha
+    // Num
+    // lowercase cha
+    // uppercase cha
+
+// display generator with answered setting
+    // 1. generate pwd btn : generated pwd in page
+    // 2. copy btn
